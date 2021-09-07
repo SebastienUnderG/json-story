f40204afa3652929f7651db46acbd65f8e7b12c3
Sebastien_G <sebastien.gestiere@gmail.com>
2021-09-07 22:08:10 +0200

    premier test

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import {ActionStory, Phase, Story} from "./phase";
+import {ActionStory, Etape, Story, Ordre} from "./phase";
 
 // module.exports = read;
 
@@ -16,7 +16,7 @@ import {ActionStory, Phase, Story} from "./phase";
  */
 
 /**
- *     label: Label;
+ * label: Label;
  * user: Label;
  * input: { label: Label, _variable: string };
  * switch: { [key: string]: string }
@@ -28,28 +28,57 @@ import {ActionStory, Phase, Story} from "./phase";
  *
  */
 
+
+// créé une suite logique d'instruction
+
+
 export class Conversation {
 
-    actions: ActionStory[];
-    story: Phase[];
+    // actions: ActionStory[];
+    // storys: Etape[];
+    story: Etape;
+    step: string[] = [];
 
 
-    constructor(actions: ActionStory[]) {
-        this.actions = actions;
+    constructor(actionsURL: string) {
+        // this.actions = require(actionsURL);
+        // faire un scan de tous les nouveaux caracteur
+        this.story = require(actionsURL);
+
+        // initialise
+        // this.init(this.story);
+        this.iterator([], this.story);
+
     }
 
-    setStory(story: Phase[]) {
-        this.story = story;
-        this.init(this.story);
+    iterator(step: string[], context: Story) {
+        let index: number = 0;
+        while (index < Ordre.length) {
+
+            this.search(step, context, Ordre[index], () => {
+                // variable ?
+                console.log("---> OK", Ordre[index]);
+
+            });
+            index++
+        }
+    }
+
+    setStory(story: Etape[]) {
+        // this.story = story;
+        // this.init(this.story);
     }
 
     talk() {
 
     }
 
-    init(context: Phase) {
-        this.search(context, 'init', () => {
+    init(context: Story) {
+        this.search(null, context, 'init', () => {
             // variable ?
+            console.log("---> OK");
+
+
         });
     }
 
@@ -58,25 +87,76 @@ export class Conversation {
     }
 
 
-    search(context: Story, key: string, callback?: any, error?: string) {
+    search(step: string[], context: Story, key: string, callback?: any, error?: string) {
         if (this.isIncludes(context, key)) {
             callback();
             // child(context[key]);
+            console.log("search ->", context[key], Object.keys(context), Object.keys(context[key]));
+
+            if (typeof context[key] === "string") {
+
+                console.log("string>>>", context[key]);
+
+                // look en local
+                let parent: Story;
+                let parent_: Story = this.story;
+                let step_: number = 0;
+                console.log("step_", step_, step[step_], step.length);
+                if (step.length > 0) {
+                    while (step_ < step.length) {
+                        console.log(this.story, parent_[step[step_]]);
+                        parent_ = parent_[step[step_]];
+                        parent = parent_;
+                        step_++;
+                        console.log("parent", parent);
+                    }
+                }
+
+
+                this.search(step, context, context[key], () => {
+                    // variable ?
+                    console.log("---> local child OK");
+
+                });
+
+                // look en child
+                // parent en Seconde position
+                this.search(step, parent, context[key], () => {
+                    // variable ?
+                    console.log("---> parent child OK");
+
+                });
+
+
+            } else if (Object.keys(context[key]).length > 0) {
+
+                // NEXT =>
+                step.push(key);
+                this.iterator(step, context[key]);
+
+            }
+
         } else {
             console.log(error ? error : ('Missing ' + key + ' KEYS'));
         }
     }
 
-    isIncludes(story: any, key: string): boolean {
-        return Object.keys(story).includes(key);
+    isIncludes(story: Story, key: string): boolean {
+        if (story) {
+            return Object.keys(story).includes(key);
+        } else {
+            return false;
+        }
+
+
     }
 
     isVariable() {
 
     }
 
-    child(context: Phase[]) {
-        Object.keys(context).forEach(e => this.search(e, context));
+    child(context: Etape[]) {
+        // Object.keys(context).forEach(e => this.search(e, context));
     }
 
 
@@ -102,3 +182,4 @@ function read(story: Map<string, any>): Observable<string> {
 */
 
 
+let exemple: Conversation = new Conversation("./exemple.json");
--- src/phase.ts
+++ src/phase.ts
@@ -1,30 +1,48 @@
-export class Phase {
-    init: Etape;
-    start: Etape;
-    restart: Etape;
-    core: Etape;
-    end: Etape;
-    endAgain: Etape;
-    terminat: Etape;
+export let Ordre: string[] = [
+    "_init",
+    "_user",
+    "_label",
+    "_message",
+    "_start",
+    "_action",
+    "_timeNext",
+    "_next"
+];
 
+
+export class GroupeLabel {
+    user: Label;
+    label: Label;
+    message: Label;
 }
 
 export class Etape {
-
-    label: Label;
-    user: Label;
-    input: { label: Label, _variable: string };
-    switch: { [key: string]: string }
-    time: number;
-    message: Label;
-    action: any;
-    next: any;
-    if: ({ [key: string]: string }| any)
+    init: Story;
+    start: Story;
+    restart: Story;
+    core: Story;
+    end: Story;
+    endAgain: Story;
+    terminat: Story;
 
 }
+
+export class Action {
+    // switch: { [key: string]: string };
+    // input: { label: Label, _variable: string };
+    action: ActionAutre;
+    timeNext: number;
+    next: Story | string;
+    // if: ({ [key: string]: string } | any);
+}
+
+export class ActionAutre {
+
+}
+
 // { [key: string]: string }
 type Label = (string | string[]);
 
-export type Story = (Phase | Etape);
+export type Story = (Etape | GroupeLabel | Action);
 
 export type ActionStory = Map<String, any>;
